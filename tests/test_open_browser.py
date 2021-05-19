fba5dc0266f76d6c84d8e2c5d16ed8e823d0a9ab
IrisOnika <IrisOnika@yandex.ru>
2021-05-19 18:33:18 +0300

    создала папку для тестов. + небольшие правки

---
+++ tests/test_open_browser.py
@@ -0,0 +1,11 @@
+import time
+
+
+def test_open_browsers(browser):
+    browser.get(browser.url)
+    # Проверка того, что это Opencart
+    browser.find_element_by_xpath("//*[text()='OpenCart']")
+    # Проверка того, что это главная страница
+    browser.find_element_by_class_name("swiper-viewport")
+    time.sleep(2) # Для демонстрации
+
